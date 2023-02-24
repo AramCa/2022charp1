1989bf6fb3962eaa3ae3a17fc2b24d8b20d55be1
AramCa <AramCagli@gmail.com>
2023-02-24 10:35:18 +0100

    Variable opdrachten

---
+++ VariableOpdracht2/VariableOpdracht2/TwitchStream.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace VariableOpdracht2
+{
+    internal class TwitchStream
+    {
+        //maak hier class variables aan:
+        // type=string, naam: name,
+        // type=string, naam: lastStreamName
+        // type=Game, naam: streamGame
+        // type=int, naam: followers
+        // type=bool, naam: automod
+
+        string name;
+        string lastStreamName;
+        Game streamGame;
+        int followers;
+        bool automod;
+
+        
+
+
+        
+    }
+}
